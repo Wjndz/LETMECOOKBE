330547f5c2b600dc96ec61491bb7237eb90f5360
Hiuth <hiuth2004@gmail.com>
2025-05-24 16:33:58 +0700

    update recipeSteps, RecipeIngredientsMapper, RecipeIngredientsService, RecipeService, RecipeStepsService

--- src/main/java/com/example/letmecookbe/mapper/RecipeIngredientsMapper.java
+++ src/main/java/com/example/letmecookbe/mapper/RecipeIngredientsMapper.java
@@ -1,7 +1,12 @@
 package com.example.letmecookbe.mapper;
 
+import com.example.letmecookbe.dto.request.IngredientsCreationRequest;
+import com.example.letmecookbe.dto.response.RecipeIngredientsResponse;
+import com.example.letmecookbe.entity.RecipeIngredients;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface RecipeIngredientsMapper {
+    RecipeIngredients toRecipeIngredients(IngredientsCreationRequest ingredients);
+    RecipeIngredientsResponse toRecipeIngredientsResponse(RecipeIngredients recipeIngredients);
 }
