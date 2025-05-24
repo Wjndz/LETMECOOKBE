75867f9fbe4dcbf486475110ab1571c99fe427b6
Hiuth <hiuth2004@gmail.com>
2025-05-24 19:18:52 +0700

    update recipeSteps, RecipeIngredientsMapper, RecipeIngredientsService, RecipeService, RecipeStepsService

--- src/main/java/com/example/letmecookbe/repository/RecipeRepository.java
+++ src/main/java/com/example/letmecookbe/repository/RecipeRepository.java
@@ -2,6 +2,8 @@ package com.example.letmecookbe.repository;
 
 import com.example.letmecookbe.entity.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RecipeRepository extends JpaRepository<Recipe, String> {
 }
